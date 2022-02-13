b6b3b89002a40a6af17ca97f239050c7f9f27edc
LikaSobolenko <91249749+LikaSobolenko@users.noreply.github.com>
2022-02-13 20:34:26 +0700

    Add files via upload

---
+++ Week 15.3 fon/assets/fon.js
@@ -0,0 +1,19 @@
+function changeMe() {
+    switch (document.getElementById('breed').value) {
+        case 'orange':
+            document.body.style.background = "orange";
+            break;
+        case 'red':
+            document.body.style.background = "red";
+            break;
+        case 'blue':
+            document.body.style.background = "blue";
+            break;
+        case 'green':
+            document.body.style.background = "green";
+            break;
+        case 'yellow':
+            document.body.style.background = "yellow";
+            break;
+    }
+}
